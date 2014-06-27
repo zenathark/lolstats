e8cee0f5edfd9a8a7471325c6ec707ab2be7bafc
Juan CarlCarlos Galan Hernandez <jcgalanhlanh@gmail.com>
2014-06-27 12:37:33 -0500

    cleaned formulae.py

--- formulae.py
+++ formulae.py
@@ -5,17 +5,17 @@ import math
 
 
 def damage_reduction(resistance):
-    """Calculates the damage reduction.
+    """calculates the damage reduction.
 
-    This function calculates the damage reduction from a given armor or magic
-    resist rating. Notice that the current equation as in patch 4.10 uses the
+    this function calculates the damage reduction from a given armor or magic
+    resist rating. notice that the current equation as in patch 4.10 uses the
     same equation from both magic and physical damage reduction.
 
-    Args:
-        resistance (float): The armor or magic resist rating.
+    args:
+        resistance (float): the armor or magic resist rating.
 
-    Returns:
-        A float that shows the percent of damage reduction from incoming
+    returns:
+        a float that shows the percent of damage reduction from incoming
         attacks
     """
     if resistance > 0:
@@ -25,20 +25,20 @@ def damage_reduction(resistance):
 
 
 def effective_health(resistance, health=100):
-    """Calculates the effective health of a champion.
+    """calculates the effective health of a champion.
 
-    This function calculates the effective health of a champios adding
-    the given armor or magic resist ratio. This function uses damage_reduction
-    function for calculating the result. If only a percent is wanted it is
+    this function calculates the effective health of a champios adding
+    the given armor or magic resist ratio. this function uses damage_reduction
+    function for calculating the result. if only a percent is wanted it is
     suggested to leave health as the default value of 100.
 
-    Args:
-        health (integer): Current health of a champion.
-        resistance: Resistance rating of a champios either armor or magic
+    args:
+        health (integer): current health of a champion.
+        resistance: resistance rating of a champios either armor or magic
         resist.
 
-    Returns:
-        A pair (e_health, ratio) where e_helath is the effective
+    returns:
+        a pair (e_health, ratio) where e_helath is the effective
         health and ratio is the health increase from having the given
         armor ratio.
     """
@@ -48,49 +48,49 @@ def effective_health(resistance, health=100):
 
 
 def resistance_reduction_flat(resistance, reduction):
-    """Calculates the resistance of a champion.
+    """calculates the resistance of a champion.
 
-    This function calculates the resistance of a champion after applying
-    a flat resistance reduction. Notice that the resultant resistance can
+    this function calculates the resistance of a champion after applying
+    a flat resistance reduction. notice that the resultant resistance can
     goes to negative values.
 
-    Args:
-        reduction (float): The reduction of the resistance.
+    args:
+        reduction (float): the reduction of the resistance.
 
-    Returns:
-        The resultant resistance after applying the reduction.
+    returns:
+        the resultant resistance after applying the reduction.
     """
     return resistance - reduction
 
 
 def resistance_reduction_percent(resistance, reduction):
-    """Calculates the resistance of a champion.
+    """calculates the resistance of a champion.
 
-    This function calculates the resistance of a champion after applying
-    a multiplicative (percent) resistance reduction. Notice that the resultant
+    this function calculates the resistance of a champion after applying
+    a multiplicative (percent) resistance reduction. notice that the resultant
     resistance can have negative values.
 
-    Args:
-        reduction (float): The percent reduction of the resistance.
+    args:
+        reduction (float): the percent reduction of the resistance.
 
-    Returns:
-        The resultant resistance after applying the reduction.
+    returns:
+        the resultant resistance after applying the reduction.
     """
     return resistance - resistance * reduction
 
 
 def resistance_penetration_flat(resistance, reduction):
-    """Calculates the resistance of a champion.
+    """calculates the resistance of a champion.
 
-    This function calculates the resistance of a champion after applying
-    a flat resistance penetration. Notice that the resultant resistance can
+    this function calculates the resistance of a champion after applying
+    a flat resistance penetration. notice that the resultant resistance can
     not go below 0.
 
-    Args:
-        reduction (float): The reduction of the resistance.
+    args:
+        reduction (float): the reduction of the resistance.
 
-    Returns:
-        The resultant resistance after applying the reduction.
+    returns:
+        the resultant resistance after applying the reduction.
     """
     if resistance - reduction > 0:
         return resistance - reduction
@@ -99,17 +99,17 @@ def resistance_penetration_flat(resistance, reduction):
 
 
 def resistance_penetration_percent(resistance, reduction):
-    """Calculates the resistance of a champion.
+    """calculates the resistance of a champion.
 
-    This function calculates the resistance of a champion after applying
-    a multiplicative(percent) resistance penetration. Notice that the
+    this function calculates the resistance of a champion after applying
+    a multiplicative(percent) resistance penetration. notice that the
     resultant resistance can not go below 0.
 
-    Args:
-        reduction (float): The reduction of the resistance.
+    args:
+        reduction (float): the reduction of the resistance.
 
-    Returns:
-        The resultant resistance after applying the reduction.
+    returns:
+        the resultant resistance after applying the reduction.
     """
     f_reduction = resistance * reduction
     if resistance - f_reduction > 0:
@@ -118,8 +118,12 @@ def resistance_penetration_percent(resistance, reduction):
         return 0
 
 
-def damage_done(damage, resistance, resistance_penetration_flat,
-                resistance_penetration_percent, resistance_reduction_flat,
-                resistance_reduction_percent):
-
-
+def damage_done(damage, resist, rpenetration_flat,
+                rpenetration_percent, rreduction_flat,
+                rreduction_percent):
+    tt_resist = resistance_reduction_flat(resist, rreduction_flat)
+    tt_resist = resistance_reduction_percent(tt_resist, rreduction_percent)
+    tt_resist = resistance_penetration_percent(tt_resist, rpenetration_percent)
+    tt_resist = resistance_penetration_flat(tt_resist, rpenetration_flat)
+    dmg = damage - tt_resist
+    return dmg
